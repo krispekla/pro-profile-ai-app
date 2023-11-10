f6533fa89328bc551ec2743f744255757f8c21a2
krispekla <peklarick@gmail.com>
2023-11-10 19:30:19 +0100

    Do some cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,9 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 
 function App() {
-	// const [user, setUser] = useState(null);
-
 	return (
 		<>
 			<h1>Pro Profile AI</h1>
-			{/* <div className="flex flex-col space-y-5 pt-8">
-			</div> */}
 			<Outlet />
 		</>
 	);
