cc0c7893810df22de7dfa72c454f1d8d91a80a8a
krispekla <peklarick@gmail.com>
2023-11-10 19:58:57 +0100

    Do some css cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { Outlet } from 'react-router-dom';
 function App() {
 	return (
 		<div className="ppai_app">
-			<nav className="">
-				<h1>Pro Profile AI</h1>
+			<nav>
+				<h1 className="logo">Pro Profile AI</h1>
 			</nav>
 			<Outlet />
 		</div>
