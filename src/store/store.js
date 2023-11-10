a6e72aab22613226640c4009af00915167ebecbd
krispekla <peklarick@gmail.com>
2023-11-10 19:31:22 +0100

    Clean unneeded store code

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,7 @@ import { create } from 'zustand';
 
 const useStore = create((set) => ({
 	session: null,
-	bears: 0,
 	setSession: (session) => set({ session }),
-	increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-	removeAllBears: () => set({ bears: 0 }),
 }));
 
 export default useStore;
