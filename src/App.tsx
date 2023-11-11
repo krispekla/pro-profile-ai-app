397f74e2e056a2c64326f15dc45c1e8eb22810e0
krispekla <peklarick@gmail.com>
2023-11-11 14:27:41 +0100

    Enable dark mode theming

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,44 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Outlet, useNavigate } from 'react-router-dom';
 
 import { Button } from '@/components/ui/button';
+import useStore from '@/store/store';
 
 function App() {
 	const navigate = useNavigate();
+	const isDarkMode = useStore((state) => state.darkMode);
 	return (
-		<div className="ppai_app">
-			<nav>
-				<h1
-					className="logo cursor-pointer"
-					onClick={() => navigate('/')}>
-					ProProfile<span className="logo_ai">AI</span>
-				</h1>
-				<div className="flex flex-row ml-auto mr-5">
-					<Button
-						variant={'secondary'}
-						onClick={() => navigate('/login')}>
-						Login
-					</Button>
-					<Button
-						className="ml-5"
-						variant={'secondary'}
-						onClick={() => navigate('/')}>
-						Logout
-					</Button>
-					<Avatar className="ml-3">
-						<AvatarImage
-							src="https://github.com/shadcn.png"
-							alt="@shadcn"
-						/>
-						<AvatarFallback>KP</AvatarFallback>
-					</Avatar>
-				</div>
-			</nav>
+		<div className={`ppai_app ${isDarkMode ? 'dark' : 'light'}`}>
 			<div className="app_container">
-				<Outlet />
+				<nav>
+					<h1
+						className="logo cursor-pointer"
+						onClick={() => navigate('/')}>
+						ProProfile<span className="logo_ai">AI</span>
+					</h1>
+					<div className="flex flex-row ml-auto mr-5">
+						<Button
+							variant={'secondary'}
+							onClick={() => navigate('/login')}>
+							Login
+						</Button>
+						<Button
+							className="ml-5"
+							variant={'secondary'}
+							onClick={() => navigate('/')}>
+							Logout
+						</Button>
+						<Avatar className="ml-3">
+							<AvatarImage
+								src="https://github.com/shadcn.png"
+								alt="@shadcn"
+							/>
+							<AvatarFallback>KP</AvatarFallback>
+						</Avatar>
+					</div>
+				</nav>
+				<main>
+					<Outlet />
+				</main>
 			</div>
 		</div>
 	);
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,11 +3,13 @@ import { create } from 'zustand';
 
 interface UserState {
 	session: Session | null;
+	darkMode: boolean;
 	setSession: (session: Session) => void;
 }
 
 const useStore = create<UserState>((set) => ({
 	session: null,
+	darkMode: false,
 	setSession: (session: Session) => set({ session }),
 }));
 
