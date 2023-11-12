8241f75a9cf82b33075ba57fb8510ab950f51e24
krispekla <peklarick@gmail.com>
2023-11-12 12:39:52 +0100

    Add color meaning for selecting ai characters

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -4,6 +4,10 @@ export default function Dashboard() {
 	return (
 		<div className="container flex min-h-full w-full flex-col p-5">
 			<CharacterModelList />
+			<div className="flex items-center justify-end">
+				<div className="w-5 h-5 rounded-full bg-yellow-300 mr-2"></div>
+				<p>Selected Character</p>
+			</div>
 		</div>
 	);
 }
