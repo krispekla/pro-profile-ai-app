8dfa3cbbf63698797e5b47a94111dcf54b219353
krispekla <peklarick@gmail.com>
2023-11-12 12:39:19 +0100

    Refactor add css for ai characters

--- src/components/CharacterModelCardAdd.tsx
+++ src/components/CharacterModelCardAdd.tsx
@@ -2,10 +2,12 @@ import { AiOutlinePlus } from 'react-icons/ai';
 
 function CharacterModelCardAdd() {
 	return (
-		<div
-			className="border-bg-slate-200 flex h-full w-52 max-w-sm cursor-pointer flex-col border-4 border-dashed hover:bg-slate-100
-		">
-			<AiOutlinePlus className="m-auto h-20 w-20 text-slate-400" />
+		<div className="flex h-full w-52 max-w-sm cursor-pointer flex-col">
+			<div
+				className="m-auto mt-3 flex h-40 w-40 rounded-full 
+				border-2 border-dashed border-secondary-foreground hover:bg-secondary">
+				<AiOutlinePlus className="m-auto h-20 w-20 text-secondary-foreground" />
+			</div>
 		</div>
 	);
 }
