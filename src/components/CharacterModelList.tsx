e0b5cac64a7291df53b91e824b02ef216c108610
krispekla <peklarick@gmail.com>
2023-11-28 19:13:35 +0100

    Update styling of titles in dashboard

--- src/components/CharacterModelList.tsx
+++ src/components/CharacterModelList.tsx
@@ -5,12 +5,11 @@ function CharacterModelList() {
 	return (
 		<section>
 			<h2 className="text-3xl font-light text-primary">
-				Your<span className="mx-1 font-bold">AI</span>
-				<span>characters:</span>
+				your<span className="mx-1 font-bold">Characters</span>
 			</h2>
 			<div
 				className="my-3 flex h-72 w-full
-						space-x-4 rounded-xl border border-secondary-foreground bg-secondary p-4
+						space-x-4 rounded-xl border border-gray-400 bg-secondary p-4
 						pb-5">
 				{[1, 2, 3].map((_, i) => (
 					<CharacterModelCard
--- src/components/PackageList.tsx
+++ src/components/PackageList.tsx
@@ -3,17 +3,17 @@ import PackageCard from './PackageCard';
 function PackageList() {
 	return (
 		<section>
-			<h2 className="text-3xl font-light text-primary">Packages</h2>
+			<h2 className="text-3xl font-bold text-primary">Packages</h2>
 			<div
-				className="my-3 flex w-full flex-col
-						 rounded-xl border border-secondary-foreground bg-secondary px-8 py-6"
+				className="my-3 flex w-full flex-col rounded-xl
+						 border border-gray-400 bg-secondary px-8 py-6 shadow-2xl shadow-gray-200"
 				style={{ minHeight: '40rem' }}>
 				<h3 className="text-2xl font-bold">Bought</h3>
 				<div className="mt-3 flex h-52 flex-row space-x-5">
 					<PackageCard bought />
 					<PackageCard bought />
 				</div>
-				<h3 className="mt-3 text-2xl font-bold">Available</h3>
+				<h3 className="mt-12 text-2xl font-bold">Available</h3>
 				<div className="mt-3 flex h-52 flex-row space-x-5">
 					<PackageCard />
 					<PackageCard />
