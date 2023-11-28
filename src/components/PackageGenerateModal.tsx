ec00d6418c090c20dcb83371fe62c1eaca89f3a9
krispekla <peklarick@gmail.com>
2023-11-28 18:36:10 +0100

    Create message for generate proccessing

--- src/components/PackageGenerateModal.tsx
+++ src/components/PackageGenerateModal.tsx
@@ -38,6 +38,7 @@ export default function PackageGenerateModal(props: PackageGenerateModalProps) {
 		});
 		setSelectedCharacters(newSelectedCharacters);
 	};
+	const [generate, setGenerate] = useState(false);
 
 	return (
 		<Dialog
@@ -46,41 +47,54 @@ export default function PackageGenerateModal(props: PackageGenerateModalProps) {
 			<DialogContent className="">
 				<DialogHeader>
 					<DialogTitle className="capitalize">Linkedin package</DialogTitle>
-					<div>
-						<h3>Select characters for which package will be used to generate imgs:</h3>
-						<div className="flex flex-wrap">
-							{selectedCharacters.map((chr, i) => (
-								<div
-									key={i}
-									className={`
-										m-1 mr-3 flex flex-col items-center justify-center
-										border-4
-										${chr.selected ? 'border-yellow-200' : 'border-transparent'}
-									 `}
-									onClick={() => setSelected(chr.id)}>
-									<div className="relative m-3 h-24 w-24 rounded-xl bg-secondary text-white">
-										<img
-											className="absolute left-0 top-0 h-full w-full object-cover"
-											src="https://avatars.githubusercontent.com/u/124599?v=4"
-											alt="Package Image"
-										/>
+					{!generate && (
+						<div>
+							<h3>Select characters for which package will be used to generate imgs:</h3>
+							<div className="flex flex-wrap">
+								{selectedCharacters.map((chr, i) => (
+									<div
+										key={i}
+										className={`
+											m-1 mr-3 flex flex-col items-center justify-center
+											border-4
+											${chr.selected ? 'border-yellow-200' : 'border-transparent'}
+										`}
+										onClick={() => setSelected(chr.id)}>
+										<div className="relative m-3 h-24 w-24 rounded-xl bg-secondary text-white">
+											<img
+												className="absolute left-0 top-0 h-full w-full object-cover"
+												src="https://avatars.githubusercontent.com/u/124599?v=4"
+												alt="Package Image"
+											/>
+										</div>
+										<div className="">{chr.name}</div>
 									</div>
-									<div className="">{chr.name}</div>
-								</div>
-							))}
+								))}
+							</div>
+							<div className="mt-8 flex items-end justify-end">
+								<Button
+									variant="default"
+									onClick={() => setGenerate(true)}>
+									Generate
+								</Button>
+							</div>
 						</div>
-						<div className="mt-8 flex items-end justify-end">
-							{/* On click generate message will be appeared with button to close modal */}
-							<Button
-								variant="default"
-								onClick={(e) => {
-									e.stopPropagation();
-									props.setOpen(false);
-								}}>
-								Generate
-							</Button>
+					)}
+					{generate && (
+						<div>
+							<div className="my-10 flex items-center justify-center">
+								<div className="mr-3">Generating</div>
+								<div className="mx-1 h-3 w-3 animate-bounce rounded-full bg-primary"></div>
+								<div className="mx-1 h-3 w-3 animate-bounce rounded-full bg-primary delay-150"></div>
+								<div className="mx-1 h-3 w-3 animate-bounce rounded-full bg-primary delay-300"></div>
+							</div>
+							<div className="mt-4 text-center">
+								Images are generated for selected characters. <br />
+								You will be notified by email when images are ready. <br />
+								You can close this modal now.
+							</div>
 						</div>
-					</div>
+					)}
 				</DialogHeader>
 			</DialogContent>
 		</Dialog>
