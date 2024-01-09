c210ef132ddc4cc1272ebd61deb7c70326e6255e
krispekla <peklarick@gmail.com>
2024-01-09 20:24:47 +0100

    Pass cover img explicitly for generated showcase in dashboard

--- src/components/PackageCard.tsx
+++ src/components/PackageCard.tsx
@@ -1,8 +1,9 @@
+import { useMemo, useState } from 'react';
+
 import { Button } from './ui/button';
 import PackageGenerateModal from './PackageGenerateModal';
 import { PackageItem } from './PackageList';
 import { useNavigate } from 'react-router';
-import { useState } from 'react';
 
 interface PackageCardProps {
 	bought?: boolean;
@@ -17,16 +18,25 @@ export default function PackageCard({
 }: PackageCardProps) {
 	const navigate = useNavigate();
 	const [open, setOpen] = useState(false);
+
+	const coverImgComputed = useMemo(() => {
+		if (typeof coverImgURL === 'undefined' && typeof pckg.CoverImgURL === 'undefined') {
+			return '';
+		}
+		return coverImgURL || pckg.CoverImgURL || '';
+	}, [coverImgURL, pckg.CoverImgURL]);
 	return (
 		<>
 			<div
 				className="relative h-52 w-72 cursor-pointer shadow-lg shadow-primary hover:-translate-y-1"
 				onClick={() => !bought && navigate('/package/5')}>
-				<img
-					className="absolute left-0 top-0 h-full w-full object-cover"
-					src={pckg.CoverImgURL}
-					alt={pckg.Description}
-				/>
+				{coverImgComputed && (
+					<img
+						className="absolute left-0 top-0 h-full w-full object-cover"
+						src={coverImgComputed}
+						alt={pckg.Description}
+					/>
+				)}
 				<div className="absolute bottom-0 left-0 right-0 top-0 flex h-full w-full items-center justify-center shadow-2xl">
 					<span className="text-2xl font-bold text-white">{pckg.Name}</span>
 					<Button
