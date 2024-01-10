d765cfc1cb61b2bab2d7aaf0039b3fddfb219e2d
krispekla <peklarick@gmail.com>
2024-01-10 20:43:49 +0100

    Update template for package overview to use real data

--- src/routes/PackageOverview.tsx
+++ src/routes/PackageOverview.tsx
@@ -1,6 +1,9 @@
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { Button } from '@/components/ui/button';
+import { PackageItem } from '@/types/package';
+import axios from '@/lib/axios';
+import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 
 export default function PackageOverview() {
@@ -18,34 +21,47 @@ export default function PackageOverview() {
 		// For example, you can navigate to a checkout page or perform an API call
 	};
 
+	const { isSuccess, data: packages } = useQuery({
+		queryKey: ['packages'],
+		queryFn: async () => axios.get('/packages/listing'),
+	});
+
+	const pckg = packages?.data.find((x: PackageItem) => x.id === parseInt(id!));
+
+	const imgsReady =
+		id &&
+		isSuccess &&
+		packages?.data.length > 0 &&
+		packages?.data.find((x: PackageItem) => x.id === parseInt(id))?.imgs.length > 0;
+
 	return (
 		<div className="mt-6 flex flex-col items-center">
-			<h1 className="mb-8 text-3xl font-bold">Linkedin package</h1>
+			<h1 className="mb-8 text-3xl font-bold">{pckg.name}</h1>
 			<div className="mb-4 flex">
 				<img
 					alt="Package"
-					src="https://fastly.picsum.photos/id/982/536/354.jpg?hmac=xXo1bhVRPwA6K0ttkJqSEghDCDNd7xWKfKpE5kqXlQo"
-					className="mr-6 h-64 w-64"
+					src={pckg.cover_img_url}
+					className="mr-6 h-64 object-contain"
 				/>
-				<p className="w-1/2 text-gray-500">
-					Enhance your linkedin profile with this package. This package includes a professional
-					photo, a professional bio, and a professional resume. Take your Linkedin profile to next
-					level. With this package, you will be able to get more job offers and more connections.
-					You will be able to get more job offers and more connections. You will be able to get more
-					job offers and more connections.
-				</p>
+				<p className="w-1/2 text-gray-500">{pckg.description}</p>
 			</div>
-			<h2 className="mt-12 text-xl">Examples:</h2>
-			<div className="mb-8 mt-3 flex flex-wrap justify-between">
-				{Array.from({ length: 27 }).map((_, index) => (
-					<img
-						key={index}
-						alt={`Example ${index + 1}`}
-						src="https://fastly.picsum.photos/id/982/536/354.jpg?hmac=xXo1bhVRPwA6K0ttkJqSEghDCDNd7xWKfKpE5kqXlQo"
-						className="mb-2 mr-2 h-32 w-32"
-					/>
-				))}
-			</div>
+			{imgsReady && (
+				<>
+					<h2 className="mt-12 text-xl">Examples:</h2>
+					<div className="mb-8 mt-3 flex flex-wrap">
+						{packages?.data
+							.find((x: PackageItem) => x.id === parseInt(id))
+							?.imgs.map((p: { url: string }, index: number) => (
+								<img
+									key={index}
+									alt={`Example ${index + 1}`}
+									src={p.url}
+									className="mb-2 mr-2 w-64 object-contain"
+								/>
+							))}
+					</div>
+				</>
+			)}
 			<div className="mb-4 flex items-center">
 				<label
 					htmlFor="quantity"
